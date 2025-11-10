34128d77eb0c8e5550a85e466737068bbef003d2
joeljogy93 <joelapp93@gmail.com>
2025-11-10 18:41:41 +1300

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,50 @@
+const CACHE_NAME = 'recipe-keeper-v1';
+const urlsToCache = [
+  '.',
+  'index.html',
+  'index.tsx',
+  'manifest.json',
+  'icon-192.png',
+  'icon-512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('Opened cache');
+        // Use { cache: 'reload' } to bypass browser cache for these requests during service worker installation.
+        const cachePromises = urlsToCache.map(urlToCache => {
+          return cache.add(new Request(urlToCache, {cache: 'reload'}));
+        });
+        return Promise.all(cachePromises);
+      })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
