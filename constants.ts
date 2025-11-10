85a4b0fbfd312ca3125a76242b18ddbd6ad471a9
joeljogy93 <joelapp93@gmail.com>
2025-11-10 18:28:47 +1300

    Create constants.ts

---
+++ constants.ts
@@ -0,0 +1,24 @@
+export const APP_NAME = 'Recipe Keeper';
+export const DRIVE_FOLDER_NAME = 'RecipeKeeper';
+export const DRIVE_RECIPES_FILENAME = 'recipes.json';
+export const DRIVE_CATEGORIES_FILENAME = 'categories.json';
+export const DRIVE_IMAGES_FOLDER_NAME = 'images';
+
+export const DEFAULT_CATEGORIES: string[] = [
+  'Appetizer',
+  'Beverage',
+  'Breakfast',
+  'Cakes',
+  'Cookies',
+  'Dessert',
+  'Filling',
+  'Kerala Non veg',
+  'Kerala veg',
+  'Main Course',
+  'Without egg cakes',
+  'Without egg cookies',
+  'Without egg dessert',
+  'Salad',
+  'Side Dish',
+  'Soup',
+];
